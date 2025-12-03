435339adc74e6317f449345ca674c5d95c1ce3c4
Telepathic Hacker <no1.coffee.addict@gmail.com>
2025-12-02 21:36:46 -0800

    Completed Part 8 Add Login Screen.

---
+++ app/src/main/java/com/example/cst338_brivera_hw03_gymlog/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.example.cst338_brivera_hw03_gymlog;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cst338_brivera_hw03_gymlog.databinding.ActivityLoginBinding;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+
+
+        setContentView(binding.getRoot());
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = MainActivity.mainActivityIntentFactory(getApplicationContext(), 0);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    static Intent loginIntentFactory(Context context) {
+        return new Intent(context, LoginActivity.class);
+    }
+}
